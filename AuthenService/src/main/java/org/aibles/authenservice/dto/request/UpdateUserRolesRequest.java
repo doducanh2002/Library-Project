c410277a8211b1d739bd9150b0e8b96a54dfb671
doducanh2002 <anh15042002hla@gmail.com>
2025-07-03 12:03:55 +0700

    update: update authen

--- AuthenService/src/main/java/org/aibles/authenservice/dto/request/UpdateUserRolesRequest.java
+++ AuthenService/src/main/java/org/aibles/authenservice/dto/request/UpdateUserRolesRequest.java
@@ -20,4 +20,20 @@ public class UpdateUserRolesRequest {
     
     @NotEmpty(message = "Roles list cannot be empty")
     private List<String> roles;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
 }
--- AuthenService/src/main/java/org/aibles/authenservice/dto/request/UpdateUserStatusRequest.java
+++ AuthenService/src/main/java/org/aibles/authenservice/dto/request/UpdateUserStatusRequest.java
@@ -1,15 +1,7 @@
 package org.aibles.authenservice.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UpdateUserStatusRequest {
     
     @NotNull(message = "User ID is required")
@@ -18,4 +10,46 @@ public class UpdateUserStatusRequest {
     private Boolean isActivated;
     private Boolean isLocked;
     private String reason;
+
+    public UpdateUserStatusRequest() {
+    }
+
+    public UpdateUserStatusRequest(String userId, Boolean isActivated, Boolean isLocked, String reason) {
+        this.userId = userId;
+        this.isActivated = isActivated;
+        this.isLocked = isLocked;
+        this.reason = reason;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Boolean getActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(Boolean activated) {
+        isActivated = activated;
+    }
+
+    public Boolean getLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(Boolean locked) {
+        isLocked = locked;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 }
